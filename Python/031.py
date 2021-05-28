7a436bb50b5d9e4173dbedfbabf2956686199b12
joonion <joonion@gmail.com>
2021-05-28 15:26:15 +0900

    Problem 31

--- Python/031.py
+++ Python/031.py
@@ -1,17 +1,21 @@
-def solve(change, coins):
-    D = [[0 for _ in range(len(coins))] for _ in range(change + 1)]
-    for i in range(change + 1):
-        D[i][0] = 1
-    for i in range(change + 1):
-        for j in range(1, len(coins)):
-            D[i][j] += D[i][j - 1]
-            if coins[j] <= i:
-                D[i][j] += D[i - coins[j]][j]
-    return D[change][len(coins) - 1]
+def search(i, wallet, change, coins):
+    W = sum(wallet)
+    if change < W:
+        return 0
+    elif change == W:
+        return 1
+    else:
+        count = 0
+        for j in range(i, len(coins)):
+            count += search(j, wallet + [coins[j]], change, coins)
+        return count
+
+def solve1(change, coins):
+    return search(0, [], change, coins)
 
 coins = [1, 2, 5, 10, 20, 50, 100, 200]
 change = 200
 # coins = [1, 2, 5]
 # change = 5
-answer = solve(change, coins)
+answer = solve1(change, coins)
 print(answer)
