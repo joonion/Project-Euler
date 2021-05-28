5b3bc7d926ef15291a20c4de23aa823df679c549
joonion <joonion@gmail.com>
2021-05-28 15:30:38 +0900

    Problem 13

--- Python/031.py
+++ Python/031.py
@@ -10,8 +10,19 @@ def search(i, wallet, change, coins):
             count += search(j, wallet + [coins[j]], change, coins)
         return count
 
+def search2(i, change, coins):
+    if change < 0:
+        return 0
+    elif change == 0:
+        return 1
+    else:
+        count = 0
+        for j in range(i, len(coins)):
+            count += search2(j, change - coins[j], coins)
+        return count
+
 def solve1(change, coins):
-    return search(0, [], change, coins)
+    return search2(0, change, coins)
 
 coins = [1, 2, 5, 10, 20, 50, 100, 200]
 change = 200
