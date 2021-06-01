a72fdd479e174f5223486dc1e6087da6cc6a1ff8
joonion <joonion@gmail.com>
2021-06-02 05:20:26 +0900

    Clean Code

--- Python/007.py
+++ Python/007.py
@@ -1,16 +1,18 @@
-def is_prime(n, primes):
-    for i in range(len(primes)):
-        if n % primes[i] == 0:
+from math import sqrt
+
+def is_prime(n):
+    for i in range(2, int(sqrt(n)) + 1):
+        if n % i == 0:
             return False
     return True
 
 def solve(nth):
-    n, primes = 3, [2]
-    while len(primes) < nth:
-        if is_prime(n, primes):
-            primes.append(n)
-        n += 2
-    return primes[-1]
+    n, count = 2, 0
+    while count < nth:
+        if is_prime(n):
+            count += 1
+        n += 1
+    return n - 1
 
 # n = 6
 n = 10001
